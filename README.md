a5085b259ded4d22fb5b8c4fd048d3e43ec63e20
WJ Carey <wjcarey_personal@pop-os.localdomain>
2020-09-25 22:20:54 -0600

    testing and debugging complete

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 Install all software needed to get a Ubuntu WordPress webserver up and running.
 
 ### Run this code below to execute the script and create a set up a ubuntu webserver
-`sudo curl -o ubuntu-webserver-quickstart.sh https://raw.githubusercontent.com/wjcarey/ubuntu-server-quickstart/master/ubuntu-webserver-quickstart.sh && sudo chmod 777 ubuntu-webserver-quickstart.sh && sudo ./ubuntu-webserver-quickstart.sh`
+`sudo curl -o ubuntu-webserver-quickstart.sh https://raw.githubusercontent.com/wjcarey/ubuntu-webserver-quickstart/master/ubuntu-webserver-quickstart.sh && sudo chmod 777 ubuntu-webserver-quickstart.sh && sudo ./ubuntu-webserver-quickstart.sh`
--- ubuntu-webserver-quickstart.sh
+++ ubuntu-webserver-quickstart.sh
@@ -16,11 +16,11 @@ else
 fi
 
 #SOFTWARE INSTALL
-apt update -qq --force-yes && apt upgrade -qq --force-yes && apt install -qq --force-yes git unzip php php-cli php-fpm php-json php-pdo php-mysql php-zip php-gd php-mbstring php-curl php-xml php-pear php-bcmath php-tokenizer php-imagick mariadb-server
+apt update -qq && apt upgrade -qq && apt install -qq git unzip php php-cli php-fpm php-json php-pdo php-mysql php-zip php-gd php-mbstring php-curl php-xml php-pear php-bcmath php-tokenizer php-imagick mariadb-server
 
 #WORDPRESS INSTALL
-INSTALL_PATH = /var/www/html
-echo "Confirm  wordpress installation in "${INSTALL_PATH}", Do you want to continue? [Y/n]"
+INSTALL_PATH="/var/www/html"
+echo "Confirm  wordpress installation in ${INSTALL_PATH}, Do you want to continue? [Y/n]"
 read CONFIRM_INSTALL_LOCATION
 if [ "$CONFIRM_INSTALL_LOCATION" != "${CONFIRM_INSTALL_LOCATION#[Yy]}" ] ;then
     rm ${INSTALL_PATH}/index.html
@@ -39,13 +39,13 @@ fi
 echo "Would you like to configure the database now? [Y/n]"
 read CONFIRM_DATABASE_CONFIGURE
 if [ "$CONFIRM_DATABASE_CONFIGURE" != "${CONFIRM_DATABASE_CONFIGURE#[Yy]}" ] ;then
-    echo "Create your username"
+    echo "Create your username ..."
     read DATABASE_USER
-    echo "create your password for "${DATABASE_USER}
+    echo "create your password for ${DATABASE_USER} ..."
     read DATABASE_PASS
-    echo "create your database Name?"
+    echo "create your database Name ..."
     read DATBASE_NAME
-    echo "User: "${DATABASE_USER}", Password: "${DATABASE_PASS}", Database: "${DATBASE_NAME}", Do you want to continue? [Y/n]"
+    echo "User: ${DATABASE_USER}, Password: ${DATABASE_PASS}, Database: ${DATBASE_NAME}, Do you want to continue? [Y/n]"
     read CONFIRM_DATABASE_INSTALL
     if [ "$CONFIRM_DATABASE_INSTALL" != "${CONFIRM_DATABASE_INSTALL#[Yy]}" ] ;then
         mysql -e "DROP USER 'root'@'localhost'; CREATE USER 'root'@'%' IDENTIFIED BY ''; GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION; CREATE USER '${DATABASE_USER}'@'%' IDENTIFIED BY '${DATABASE_PASS}'; GRANT ALL PRIVILEGES ON *.* TO '${DATABASE_USER}'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES;"
